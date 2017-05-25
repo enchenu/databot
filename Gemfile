1d34ec2a52bb4809e1e3cda68ade3a3dee1960ac
Enrique Chenú <enchenu@gmail.com>
2017-05-24 23:04:53 -0400

    Services created

--- Gemfile
+++ Gemfile
@@ -4,10 +4,11 @@ gem "activerecord"
 gem "sinatra-activerecord"
 gem 'pg'
 gem 'rake'
+gem 'pg_search'
 
 group :development do
  gem "tux"
- 
+
 end
 
 group :test do
---
+++ app/services/faq/create_service.rb
@@ -0,0 +1,29 @@
+module FaqModule
+  class CreateService
+    def initialize(params)
+      # TODO: identify origin and set company
+      @company = Company.last
+      @question = params["question.original"]
+      @answer = params["answer.original"]
+      @hashtags = params["hashtags.original"]
+    end
+
+    def call
+      if @hashtags == nil || @hashtags == ""
+        return "Hashtag Obrigatória"
+      end
+
+      begin
+      Faq.transaction do
+        faq = Faq.create(question: @question, answer: @answer, company: @company)
+        @hashtags.split(/[\s,]+/).each do |hashtag|
+          faq.hashtags << Hashtag.create(name: hashtag)
+        end
+      end
+      "Criado com sucesso"
+      rescue
+        "Problemas na criacao"
+      end
+    end
+  end
+end
